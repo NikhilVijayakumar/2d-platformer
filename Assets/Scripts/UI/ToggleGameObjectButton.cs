016d6314a28cca0314aa86013fb6005f69644baa
Nikhil V <nikhil.vijaykumar@gmail.com>
2020-10-03 15:31:34 -0700

    add intro level2 and win scene

---
+++ Assets/Scripts/UI/ToggleGameObjectButton.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ToggleGameObjectButton : MonoBehaviour
+{
+    public const string k_ButtonNameCancel = "Cancel";
+    public GameObject objectToToggle;    
+    public bool resetSelectionAfterClick;
+
+    void Update()
+    {
+        if (objectToToggle.activeSelf && Input.GetButtonDown(k_ButtonNameCancel))
+        {
+            SetGameObjectActive(false);
+        }
+    }
+
+    public void SetGameObjectActive(bool active)
+    {
+        objectToToggle.SetActive(active);
+      
+        if (resetSelectionAfterClick)
+            EventSystem.current.SetSelectedGameObject(null);
+    }
+}
